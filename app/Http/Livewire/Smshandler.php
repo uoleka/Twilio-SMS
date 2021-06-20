74a62fd23cb6a364cd36ee112bd9d1d5e386dd25
Ugochukwu Oleka <ugo@corfee.uk>
2021-06-20 15:17:43 +0100

    sms hanlder component and form

---
+++ app/Http/Livewire/Smshandler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Smshandler extends Component
+{
+    public function render()
+    {
+        return view('livewire.sms_handler');
+    }
+}
